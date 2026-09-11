22d34347a6de6f605cbfcd8729bd89a121784e90
GabrielCaligiuri <gabecaligiuri@gmail.com>
2026-09-10 18:51:00 -0700

    Updated Background Page
    Added Caffe Calabria to work experience
    Updated relevant coursework
    Refine core competencies
    Fix grammar and formatting

--- src/Background.jsx
+++ src/Background.jsx
@@ -8,15 +8,15 @@ export default function Background() {
         <div className='section_block'>
             <h2 className='education__title'> Education </h2>
             <ul className='list__education'>
-                <p className='coursework'> Relevant coursework: Data structures, Computer Architecture and Assembly, Python Programming, Programming Fundementals I and II.</p>
+                <p className='coursework'> Relevant coursework: Data Structures and Algorithms, Discrete Math, Applied Linear Algebra, Probability and Statistics, Information Assurance.</p>
                 <li>
                     <h3 className='cc__subcategory'> Palomar Community College: </h3>
                     <p className='cc__information'>
-                        Bachelor of Science, Computer Science, Sep 2022 - May 2025
+                        Computer Science Transfer Program, Sep 2022 - May 2025
                     </p>
                 </li>
                 <li>
-                    <h3 className='asu__subcategory'> Arizona State University</h3>
+                    <h3 className='asu__subcategory'> Arizona State University: </h3>
                     <p className='asu__information'>
                         Bachelor of Science, Computer Science, Sep 2025 - Present
                     </p>
@@ -35,25 +35,55 @@ export default function Background() {
                 <li>
                     <h3 className='web'> Web Development </h3>
                     <p className='web__info'>
-                        HTML, CSS, JavaScript, TypeScript, Git, Github, React
+                        HTML, CSS, JavaScript, TypeScript, Git, GitHub, React
                     </p>
                 </li>
                 <li>
                     <h3 className='core'> Core Competencies </h3>
                     <p className='core__info'>
-                        Fast Learner, Technical Literacy, Problem Solving, Attention to Detail
+                        Statistical Analysis, Problem Solving, Attention to Detail, Data Analysis
                     </p>
                 </li>
             </ul>
         </div>
         <div className='section_block'>
+            <h2 className='skills'> Work Experience </h2>
             <ul className='list__work'>
                 <li>
-                    <h3 className='jojos'> Jojos Creamery </h3>
-                    <p className='jojos__info'>
-                        Managed store operations, including inventory tracking and cash handling, overseeing a team of 4 employees
-                        in a collaborative work environment.
-                    </p>
+                    <h3 className='calabria'> Caffe Calabria </h3>
+                        <ul>
+                            <li>
+                                <p className='calabria__info'>
+                                    Designed and built a wholesale ordering portal enabling customers to place orders online.
+                                </p>
+                            </li>
+                            <li>
+                                <p className='calabria__info'>
+                                    Developed and maintained the back end of the company website resolving issues and implementing updates as needed.
+                                </p>
+                            </li>
+                            <li>
+                                <p className='calabria__info'>
+                                    Supported wholesale operations directly, including roasting and packing coffee beans to fulfill orders.
+                                </p>
+                            </li>
+                        </ul>
+                </li>
+                <li>
+                    <h3 className='jojos'> JoJo's Creamery </h3>
+                    <ul>
+                        <li>
+                            <p>
+                                Managed store operations, including inventory tracking and cash handling, overseeing a team of 4 employees.
+                            </p>
+                        </li>
+                        <li>
+                            <p className='jojos__info'>
+                                Led a team of 4 employees, training new hires and maintaining efficiency, cleanliness and product quality.
+                            </p>
+                        </li>
+                        
+                    </ul>
                 </li>
             </ul>
         </div>
