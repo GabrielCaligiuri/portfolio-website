f60838c5472441d522862bede6f77101d7b32eff
GabrielCaligiuri <gabecaligiuri@gmail.com>
2026-09-10 18:14:13 -0700

    Update Projects and Project order
    Added Valorant Headshot Statistical Analysis and reordered projects so that they are correctly newest first.

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -3,10 +3,10 @@
 import './projects.css';
 
 const projects = [
-  { label: "View Project →", title: "Chess Game" , href: "https://github.com/GabrielCaligiuri/projects/tree/main/chessJava", description: "A playable Java chess game, built for 2 local players. Features complete piece movement, game rule enforcement, and clean UI components."},
-  { label: "View Project →", title: "Portfolio Website", href: "https://github.com/GabrielCaligiuri/portfolio-website-src", description: "Custom portfolio website built with React and Vite. Features a clean responsive layout, reusable React components and smooth navigation. "}
+  { label: "View Project →", title: "Valorant Headshot Statistical Analysis" , href: "https://github.com/gabrielcaligiuri/valorant-headshot-statistical-analysis", description: "Statistical analysis of 20,000 Valorant matches comparing weekday vs weekend headshot percentages across 4 phases using Python, pandas, numpy, and scipy."},
+  { label: "View Project →", title: "Portfolio Website", href: "https://github.com/gabrielcaligiuri/portfolio-website", description: "Custom portfolio website built with React and Vite. Features a clean responsive layout, reusable React components and smooth navigation. "},
+  { label: "View Project →", title: "Chess Game" , href: "https://github.com/gabrielcaligiuri/chess-game", description: "A playable Java chess game, built for 2 local players. Features complete piece movement, game rule enforcement, and clean UI components."}
 ]
-
 export default function Projects() {
     return (
       <div className="project_container">
